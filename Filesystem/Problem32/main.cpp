354b9c72596c5de282a33c7e653aee5cdc61c127
으나 :) <euna501@gmail.com>
2018-11-09 12:11:00 +0900

    32번 문제

--- Filesystem/Problem32/main.cpp
+++ Filesystem/Problem32/main.cpp
@@ -1,4 +1,4 @@
-// 32. Pascalʼs triangle
+﻿// 32. Pascalʼs triangle
 // Write a function th#t prints up to 10 rows of Pascalʼs triangle to the console.
 // 32. 파스칼의 삼각형
 // 콘솔로 파스칼의 삼각형 10행을 출력하는 함수를 작성하라.
@@ -8,9 +8,33 @@
 #define CATCH_CONFIG_MAIN   
 #include "catch2/catch.hpp"
 
+
+
 // Redirect CMake's #define to C++ constexpr string
 constexpr auto TestName = PROJECT_NAME_STRING;
 
-TEST_CASE("tokenize", "[Joining strings]") {
+void pascalTriangle(int line)
+{
+	int coef = 1;
 
+	for (int i = 0; i < line; i++)
+	{
+		for (int space = 1; space <= line - i; space++)
+			std::cout << "  ";
+
+		for (int j = 0; j <= i; j++)
+		{
+			if (j == 0 || i == 0)
+				coef = 1;
+			else
+				coef = coef * (i - j + 1) / j;
+
+			std::cout << coef << "   ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+TEST_CASE("Pascalʼs triangle") {
+	pascalTriangle(10);
 }

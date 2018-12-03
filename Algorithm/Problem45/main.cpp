8158a8de82d739e679f020d17606125a1231b8f7
으나 :) <euna501@gmail.com>
2018-12-03 21:17:13 +0900

    45번 문제

--- Algorithm/Problem45/main.cpp
+++ Algorithm/Problem45/main.cpp
@@ -1,6 +1,35 @@
-#include <gsl/gsl>
+#include "gsl/gsl"
 
-int main(int argc, char* argv[])
-{
-    return 0;
+// create `main` function for catch
+#define CATCH_CONFIG_MAIN   
+#include "catch2/catch.hpp"
+
+// Redirect CMake's #define to C++ constexpr string
+constexpr auto TestName = PROJECT_NAME_STRING;
+
+#include <queue>
+
+template<typename T> void print_queue(T& q) {
+    while(!q.empty()) {
+        std::cout << q.top() << " ";
+        q.pop();
+    }
+    std::cout << '\n';
+}
+
+TEST_CASE("Priority queue") {
+    std::priority_queue<int> q;
+
+    REQUIRE(q.empty() == true);
+
+    for(int n : {1,8,5,6,3,4,0,9,7,2})
+    {
+        q.push(n);
+    }
+
+    REQUIRE(q.size() == 10);
+
+    print_queue(q);
+
+    REQUIRE_FALSE(q.size() == 10);
 }
